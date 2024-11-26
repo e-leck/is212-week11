93b3c92334d07375c5a993f02ae82cbed2324680
e-leck <elvy.leck.2022@scis.smu.edu.sg>
2024-11-26 22:04:21 +0800

    ex3

--- ex3/ex3_smelly.py
+++ ex3/ex3_smelly.py
@@ -19,10 +19,10 @@ class Electronics(Product):
     def __init__(self, name, price):
         super().__init__(name, price, discount_rate=0.10, tax_rate=0.15)
 
-class Electronics(Product):
+class Clothing(Product):
     def __init__(self, name, price):
         super().__init__(name, price, discount_rate=0.20, tax_rate=0.08)
 
-class Electronics(Product):
+class Grocery(Product):
     def __init__(self, name, price):
         super().__init__(name, price, discount_rate=0.05, tax_rate=0.02)
