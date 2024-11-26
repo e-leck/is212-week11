1952fca5c2008f5a1dea0afb507c30a067e9748f
e-leck <elvy.leck.2022@scis.smu.edu.sg>
2024-11-26 21:35:32 +0800

    deodorise ex2_smelly final

--- ex2/ex2_smelly.py
+++ ex2/ex2_smelly.py
@@ -5,19 +5,19 @@ class Shape:
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-    def calculate_area(radius):
+    def calculate_area(self, radius):
         return 3.14 * radius * radius
     
 class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
         self.width = width
-    def calculate_area(self, length, width):
-        return length * width
+    def calculate_area(self):
+        return self.length * self.width
     
 class Triangle(Shape):
     def __init__(self, base, height):
         self.base = base
         self.height = height
-    def calculate_area(self, base, height):
-        return 0.5 * base * height
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
