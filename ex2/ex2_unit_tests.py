b7779c7dd40bc075a05e20854c8bb4bf7e427a31
e-leck <elvy.leck.2022@scis.smu.edu.sg>
2024-11-26 21:41:33 +0800

    ex2_unit_tests final

--- ex2/ex2_unit_tests.py
+++ ex2/ex2_unit_tests.py
@@ -5,17 +5,17 @@ class TestShape(unittest.TestCase):
 
     def test_calculate_area_circle(self):
         # Test for a circle with radius 5
-        result = Circle(radius=5).calculate_area
+        result = Circle(radius=5).calculate_area()
         self.assertAlmostEqual(result, 78.5, places=2)
 
     def test_calculate_area_rectangle(self):
         # Test for a rectangle with length 4 and width 6
-        result = Rectangle(length=4, width=6).calculate_area
+        result = Rectangle(length=4, width=6).calculate_area()
         self.assertEqual(result, 24)
 
     def test_calculate_area_triangle(self):
         # Test for a triangle with base 10 and height 8
-        result = Triangle(base=10, height=8).calculate_area
+        result = Triangle(base=10, height=8).calculate_area()
         self.assertEqual(result, 40)
 
 
